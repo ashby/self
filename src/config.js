d2985aa41896b929c598636408be7fef575e9b58
ash <ashbyutting@gmail.com>
2019-04-12 17:23:08 -0500

    add ext

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const ENV = 'dev'
 const SET_ENV = ENV !== 'prod' && `${ENV}.` || '';
-export const HIGHER_POWER_PATH = `http://${SET_ENV}higher-power`;
+export const HIGHER_POWER_PATH = `http://${SET_ENV}higher-power.api.cloud`;
